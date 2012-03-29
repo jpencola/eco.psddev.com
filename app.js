b0c3726ca9c4ce2a187535829ec61953a987bdc1
Erik Zettersten <erik@zettersten.com>
2012-03-29 16:22:36 -0400

    removing old

--- app.js
+++
@@ -1,7 +0,0 @@
-var config = require('./config.js');
-
-// Setup Express Configs
-require("./libs/express.js").init(config);
-
-// Startup Server Configs
-require("./libs/server.js")(config);
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env : "developement",
-  port : 8000,
-  version : "0.2.2"
-};
